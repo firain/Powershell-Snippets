7b4529ece52fa2439beec925b22e8c6bdcca0df4
firain <pla15@live.com>
2024-11-15 13:54:38 -0800

    Update ConvertFrom-Ini.ps1
    add the code for handling string array and raw string, comment out the test code

--- ConvertFrom-Ini.ps1
+++ ConvertFrom-Ini.ps1
@@ -7,9 +7,10 @@ function ConvertFrom-Ini {
     This function takes a string containing INI file content and converts it into a PowerShell object for easy access to the data.
     
     .PARAMETER InputObject
-    The content of the INI file as a string.
+    The content of the INI file as a string or string array.
     
     .EXAMPLE
+    # Data from ini Wikipedia
     $iniString = @"
     ; last modified 1 April 2001 by John Doe
     [owner]
@@ -30,30 +31,50 @@ function ConvertFrom-Ini {
     #>
     [CmdletBinding()]
     param (
-        [Parameter(ValuefromPipeline=$True)]
-        [string]$InputObject
+        [Parameter(ValuefromPipeline = $True)]
+        $InputObject
     )
-
-    $output = [PSCustomObject]@{}
-    $section = $null
-
-    #convert raw string to string array
-    if ($InputObject -is [String]){
-        $InputObject = $InputObject -split "`n"
+    begin {
+        $script:output = [PSCustomObject]@{}
+        $script:section = $null
+        $script:ProcessString = [System.Collections.Arraylist]@()
     }
-    
-    $InputObject | ForEach-Object {
-        $_ = $_.Trim()
-        if ($_ -match "^\s*;") { return }  # Ignore comments
-        if ($_ -match "^\[(.+?)\]") {
-            $section = $matches[1]
-            $output | Add-Member -MemberType NoteProperty -Name $section -Value ([PSCustomObject]@{})
-        }
-        elseif ($section -and ($_ -match "^(.*?)\s*=\s*(.*)")) {
-            $key, $value = $matches[1].Trim(), $matches[2].Trim('"')
-            $output.$section | Add-Member -MemberType NoteProperty -Name $key -Value $value
-        }
+    process {
+        $script:ProcessString.add($InputObject) | Out-Null
     }
+    end {
+        #handles if the string is a raw content format ex: `cat file -raw`
+        if($script:ProcessString.Count -eq 1) {$script:ProcessString = $script:ProcessString[0] -split "`n"}
+        
+        $script:ProcessString | ForEach-Object {
+            $_ = $_.Trim()
+            if ($_ -match "^\s*;") { return }  # Ignore comments
+            if ($_ -match "^\[(.+?)\]") {
+                $script:section = $matches[1]
+                $script:output | Add-Member -MemberType NoteProperty -Name $script:section -Value ([PSCustomObject]@{})
+            }
+            elseif ($script:section -and ($_ -match "^(.*?)\s*=\s*(.*)")) {
+                $key, $value = $matches[1].Trim(), $matches[2].Trim('"')
+                $script:output.$script:section | Add-Member -MemberType NoteProperty -Name $key -Value $value
+            }
+        }
 
-    return $output
+        return $script:output
+    }
 }
+
+
+# $iniString = @"
+# ; last modified 1 April 2001 by John Doe
+# [owner]
+# name = John Doe
+# organization = Acme Widgets Inc.
+
+# [database]
+# server = 192.0.2.62
+# port = 143
+# file = "payroll.dat"
+# "@
+
+# cat .\test.ini | ConvertFrom-Ini
+# $iniString | ConvertFrom-Ini
